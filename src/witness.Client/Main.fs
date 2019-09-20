a7f4e10eaa75c2bd4c6b9ecbb1fa89447045d4cf
shinkwhek <klklpshin@gmail.com>
2019-09-21 00:57:39 +0900

    add mode

--- src/witness.Client/Main.fs
+++ src/witness.Client/Main.fs
@@ -9,22 +9,30 @@ open PazzleRender
 
 let inline (|?) x y = List.append x y
 
+/// ---- ---- model ---- ----
+
+type Mode =
+  | Nothing
+  | PathDraw of entrypoint: Point
+
 type Model =
-  { x : int
-    grid : Grid
+  { grid : Grid
+    mode : Mode
     pathes : Path list
     elements : Element list }
 
 let initModel =
-  { x = 0
-    grid =
+  { grid =
       { pixelWidth = 500.
         pixelHeight = 500.
         width = 5
         height = 5 }
+    mode = Nothing
     pathes = []
     elements = [ Entry {row=0.0; column=0.0}
                  Goal {row=0.0; column=4.0} ] }
+
+/// ---- ---- message ---- ----
 
 type IncDec =
   | Width
@@ -33,6 +41,7 @@ type IncDec =
 type Message =
   | Inclease of IncDec
   | Declease of IncDec
+  | Mode of Mode
 
 let update message model =
   match message with
@@ -40,12 +49,33 @@ let update message model =
   | Inclease Height -> { model with grid = { model.grid with height = model.grid.height + 1 } }
   | Declease Width -> { model with grid = { model.grid with width = model.grid.width - 1 } }
   | Declease Height -> { model with grid = { model.grid with height = model.grid.height - 1 } }
+  | Mode m -> { model with mode = m }
+
+/// ---- ---- view ---- ----
+
+let renderElements dispatch elements grid =
+  seq {
+    for element in elements do
+      match element with
+      | Entry p ->
+        yield renderEntry [ on.click (fun _ -> dispatch (Mode (PathDraw p))) ] p <| gridOffset grid
+      | Goal p ->
+        yield renderGoal p <| gridOffset grid
+  } |> Seq.toList
+
+let renderUserPath dispatch mode grid =
+  match mode with
+  | Nothing -> []
+  | PathDraw p ->
+    [ renderEntry [ "fill" => color2str White ] p <| gridOffset grid ]
 
 let view model dispatch =
+  let mode = model.mode
   let grid = model.grid
   let elements = model.elements
   let render = (grid |> renderGrid)
-                |? (grid |> renderElements elements)
+                |? (grid |> renderElements dispatch elements)
+                |? (grid |> renderUserPath dispatch mode)
   div []
     [ div [] [ text <| "pixelWidth: " + string grid.pixelWidth ]
       div [] [ text <| "pixelHeight: " + string grid.pixelHeight ]
--- src/witness.Client/PazzleRender.fs
+++ src/witness.Client/PazzleRender.fs
@@ -17,13 +17,15 @@ let inline localPosition p grid =
   let y = step * (p.row + 1.0)
   x, y, step
 
-let renderLine (p1: Point) (p2: Point) (color: Color) (grid: Grid) =
+let renderLine attr (p1: Point) (p2: Point) (color: Color) (grid: Grid) =
   let color = color2str color
   let x1, y1, step = localPosition p1 grid
   let x2, y2, _ = localPosition p2 grid
-  line [ "stroke" => color
-         "stroke-width" => step * 0.2
-         "stroke-linecap" => "round" ] x1 y1 x2 y2
+  let attr =
+    List.append [ "stroke" => color
+                  "stroke-width" => step * 0.2
+                  "stroke-linecap" => "round" ] attr
+  line attr x1 y1 x2 y2
 
 /// ---- ---- render grid ---- ----
 
@@ -36,28 +38,18 @@ let renderGrid grid =
                  "ry" => 10 ] 0 0 grid.pixelWidth grid.pixelHeight
     for i in [startX..endX] do
       let i = float i
-      yield renderLine {row=0.0; column=i} {row=float endY; column=i} Black <| gridOffset grid
+      yield renderLine [] {row=0.0; column=i} {row=float endY; column=i} Black <| gridOffset grid
     for j in [startY..endY] do
       let j = float j
-      yield renderLine {row=j; column=0.0} {row=j; column=float endX} Black <| gridOffset grid
+      yield renderLine [] {row=j; column=0.0} {row=j; column=float endX} Black <| gridOffset grid
   } |> Seq.toList
 
 /// ---- ---- render elements ---- ----
 
-let inline renderEntry p grid =
+let inline renderEntry attr p grid =
   let x, y, step = localPosition p grid
   let r = step * 0.25
-  circle [] x y r
+  circle attr x y r
 
 let inline renderGoal p grid =
-  renderLine p { p with column = p.column + 0.3 } Black grid
-
-let renderElements elements grid =
-  seq {
-    for element in elements do
-      match element with
-      | Entry p ->
-        yield renderEntry p <| gridOffset grid
-      | Goal p ->
-        yield renderGoal p <| gridOffset grid
-  } |> Seq.toList
+  renderLine [] p { p with column = p.column + 0.3 } Black grid
